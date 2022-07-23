8df7945d445e3a6d9fe4a2d3cfc60a8ec00cf234
Adrian Maćkowiak <adrianmackowiak1@wp.pl>
2022-07-23 21:11:28 +0200

    Add flutterfireUI

--- lib/features/details/cubit/details_cubit.dart
+++ lib/features/details/cubit/details_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:cantwait28/models/item_model.dart';
 import 'package:cantwait28/repositories/items_repository.dart';
-import 'package:meta/meta.dart';
+
 
 part 'details_state.dart';
 
